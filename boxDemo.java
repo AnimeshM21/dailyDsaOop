9b2b0a5883f1622de5b6fb90044c5f96ff70609e
AnimeshM21 <animesh.mishra2023@gmail.com>
2024-09-05 02:38:25 +0530

    Udated Boxdemo code

--- boxDemo.java
+++ boxDemo.java
@@ -5,8 +5,11 @@ class Box{
     void volume(){
         System.out.println("Volume of the Box is "+width*height*depth);
     }
-    void sum(){
-        System.out.println("Sum of the Box is "+(width+height+depth));
+    void setDim(double w, double h,double d){
+        width =w;
+        height = h;
+        depth = d;
+
     }
 
 }
@@ -16,16 +19,14 @@ public class boxDemo {
     public static void main(String args[]){
         Box newBox1 = new Box();
         Box newBox2 = new Box();
-        
-        newBox1.width = 10;
-        newBox1.height = 200;
-        newBox1.depth = 15;
-        newBox2.width = 10;
-        newBox2.height = 20;
-        newBox2.depth = 15;
-        
+        newBox1.setDim(10, 20, 15);
+        newBox2.setDim(3, 6, 9);
         newBox1.volume();
-        newBox2.sum();
+        newBox2.volume();
+        
+       
+        
+        
 
     }
     /*Box b1 = new Box();
@@ -33,5 +34,7 @@ public class boxDemo {
     here b2 is assigned to b1 object, but no separate space is allocated to b2. b2 is a reference to the same memory location as of b1.
  */
     
+ /*Constructors have no return type, not even void. This is because the implicit return type of a class’ constructor is the class type itself. It is the constructor’s job to initialize the internal state of an object so that the code creating an instance will have a fully initialized, usable object immediately.
+ */
     
 }
