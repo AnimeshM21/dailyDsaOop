7b26bbf44b57c2c81c4e9ef6eb35a0669377126b
AnimeshM21 <animesh.mishra2023@gmail.com>
2024-09-05 02:17:11 +0530

    Updated BoxDemo

--- boxDemo.java
+++ boxDemo.java
@@ -8,15 +8,25 @@ class Box{
 
 public class boxDemo {
     public static void main(String args[]){
-        Box newBox = new Box();
-        double volume;
-        newBox.width = 10;
-        newBox.height = 200;
-        newBox.depth = 15;
-        volume = newBox.width * newBox.height * newBox.depth;
-        System.out.println("Volume of the Box is "+volume);
+        Box newBox1 = new Box();
+        Box newBox2 = new Box();
+        double volume1;
+        double sum;
+        newBox1.width = 10;
+        newBox1.height = 200;
+        newBox1.depth = 15;
+        newBox2.width = 10;
+        newBox2.height = 20;
+        newBox2.depth = 15;
+        volume1 = newBox1.width * newBox1.height * newBox1.depth;
+        sum = newBox2.width + newBox2.height + newBox2.depth;
+        System.out.println("Volume of the Box is "+volume1);
+        System.out.println("Sum of the Box is "+sum);
     }
-
+    /*Box b1 = new Box();
+    Box b2 = b1;
+    here b2 is assigned to b1 object, but no separate space is allocated to b2. b2 is a reference to the same memory location as of b1.
+ */
     
     
 }
