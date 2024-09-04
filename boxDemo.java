2a99094a7f8761a6524756d09eef33c9d553da30
AnimeshM21 <animesh.mishra2023@gmail.com>
2024-09-05 02:20:17 +0530

    Method added

--- boxDemo.java
+++ boxDemo.java
@@ -2,6 +2,12 @@ class Box{
     double width;
     double height;
     double depth;
+    void volume(){
+        System.out.println("Volume of the Box is "+width*height*depth);
+    }
+    void sum(){
+        System.out.println("Sum of the Box is "+(width+height+depth));
+    }
 
 }
 
@@ -10,18 +16,17 @@ public class boxDemo {
     public static void main(String args[]){
         Box newBox1 = new Box();
         Box newBox2 = new Box();
-        double volume1;
-        double sum;
+        
         newBox1.width = 10;
         newBox1.height = 200;
         newBox1.depth = 15;
         newBox2.width = 10;
         newBox2.height = 20;
         newBox2.depth = 15;
-        volume1 = newBox1.width * newBox1.height * newBox1.depth;
-        sum = newBox2.width + newBox2.height + newBox2.depth;
-        System.out.println("Volume of the Box is "+volume1);
-        System.out.println("Sum of the Box is "+sum);
+        
+        newBox1.volume();
+        newBox2.sum();
+
     }
     /*Box b1 = new Box();
     Box b2 = b1;
