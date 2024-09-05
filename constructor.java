afd16b413561b4ff326285817b7799a76988fd63
AnimeshM21 <animesh.mishra2023@gmail.com>
2024-09-05 11:17:59 +0530

    constructor code updated

--- constructor.java
+++ constructor.java
@@ -34,10 +34,7 @@ public class constructor {
         
 
     }
-    /*Box b1 = new Box();
-    Box b2 = b1;
-    here b2 is assigned to b1 object, but no separate space is allocated to b2. b2 is a reference to the same memory location as of b1.
- */
+
     
  /*Constructors have no return type, not even void. This is because the implicit return type of a class’ constructor is the class type itself. It is the constructor’s job to initialize the internal state of an object so that the code creating an instance will have a fully initialized, usable object immediately.
  */
