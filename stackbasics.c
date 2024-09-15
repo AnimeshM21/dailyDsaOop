0bf785fc5a9d4e74fc0ecde43c1b769f231ccfde
AnimeshM21 <animesh.mishra2023@gmail.com>
2024-09-16 03:53:42 +0530

    Stack Basics corrected

--- stackbasics.c
+++ stackbasics.c
@@ -15,21 +15,23 @@ void newStack(STACK *stack){
 int isFull(STACK *stack){
     if(stack->top == MAX-1){
         printf("Stack is Full. Get Fucked!!!");
-        return stack->top == MAX-1;
+        return 1;
     }
+    return 0;
 }
 int isEmpty(STACK *stack){
     if(stack->top == -1){
         printf("Stack is Empty. Get Fucked!!!");
-        
+        return 1;
     }
+    return 0;
 }
 
 int push(STACK *stack,int item){
     if(isFull(stack)){
         printf("The Stack is full try popping first");
     }else{
-        stack->items[++(stack->top)] == item;
+        stack->items[++(stack->top)] = item;
         printf("Successfully pushed %d to the Stack \n",item);
     }
 }
@@ -39,6 +41,28 @@ int pop(STACK *stack){
         printf("Sorry but the stack is empty!");
     }else{
         int popped = stack->items[(stack->top)--];
-        printf("The popped item is %d: ",popped);
+        printf("The popped item is ---%d---!! ",popped);
     }
+}
+
+int main() {
+    STACK stack;
+    newStack(&stack);
+    
+    
+    push(&stack, 7);
+    push(&stack, 21);
+    push(&stack, 3);
+    
+    
+    // for(int i = 0; i < MAX; i++) {
+    //     push(&stack, i);
+    // }
+    pop(&stack);
+    
+    isEmpty(&stack);
+    isFull(&stack);
+    
+    printf("the Stack is :%d",stack);
+    return 0;
 }
