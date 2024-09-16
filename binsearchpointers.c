c1dde0dda17d836ff952abc83d5747b3c50a8601
AnimeshM21 <animesh.mishra2023@gmail.com>
2024-09-17 00:31:49 +0530

    BInary Search updated

--- binsearchpointers.c
+++ binsearchpointers.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #define MAX 10
-void BinSea(int arr[MAX],int *pFind,int *pFirst, int *pLast){
+int BinSea(int arr[MAX],int *pFind,int *pFirst, int *pLast){
    
     if (*pFirst>*pLast){
         printf("Not a sorted list");
         return ;
     }
     int pMid;
-    pMid  =  MAX/2;
-    int pWalk ;
-    pWalk = &arr[1];
+    pMid  =  (*pFirst+*pLast)/2;
+    
 
     while(*pFirst<= *pLast){
         if(*pFind == arr[pMid]){
+            return pMid;
         }
         else if(*pFind>arr[pMid]){
             *pFirst = arr[pMid+1];
@@ -27,24 +27,20 @@ void BinSea(int arr[MAX],int *pFind,int *pFirst, int *pLast){
 
 int main(){
 
-int myarray;
-printf("Enter a 10 digits array:");
-scanf("%d",&myarray);
+int myarray[MAX] ={1,2,3,4,5,6,7,8,9,10} ;
+
 
 int target;
-
 printf("Enter element to find");
 scanf("%d",&target);
 
 
-int *first;
-printf("Enter first element of the array");
-scanf("%d",&first);
+int first = 0;
 
-int *last;
-printf("Enter last element of the array");
-scanf("%d",&last);
 
-BinSea(myarray,target,first,last);
+int last = MAX-1;
 
+
+int res = BinSea(myarray,&target,&first,&last);
+return res;
 }
