1bc3cb4cd76af2ca902d4f77ad63fe2a0a0bb758
AnimeshM21 <animesh.mishra2023@gmail.com>
2024-09-17 00:57:28 +0530

    LAB5_2

---
+++ LAB5_2.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 100
+int Stack[MAX];
+int top= -1;
+
+void push(int val){
+
+Stack[++top] = val;
+}
+
+int pop(){
+    return Stack[top--];
+
+}
+
+void decToBin(int n){
+
+while(n>0){
+    int rem = n%2;
+    push(rem);
+    n = n/2;
+
+}
+printf("Decimal to Binary:");
+while(top>=0){
+    printf("%d",pop());
+}
+
+}
+
+int main(){
+    int decimal= 8;
+    printf("Decimal to Binary conversion is:");
+    decToBin(decimal);
+
+}
+
+
+
