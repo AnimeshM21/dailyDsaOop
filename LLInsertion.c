bc481e339d3980d4704afe1d3cb19ef685ad31aa
AnimeshM21 <animesh.mishra2023@gmail.com>
2024-09-21 15:11:16 +0530

    Insertion to LL complete

--- LLInsertion.c
+++ LLInsertion.c
@@ -1,32 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct{
+typedef struct
+{
     int data;
     struct NODE *next;
     int prev;
-}NODE;
+} NODE;
 
+//Function to traverse the LL i.e show every value in the Linked-List
+void LLTraversal(NODE *ptr)
+{
 
-void LLTraversal(NODE *ptr){
-
-    while(ptr!=NULL){
-        printf("Element: %d \n",ptr->data);
+    while (ptr != NULL)
+    {
+        printf("Element: %d \n", ptr->data);
         ptr = ptr->next;
     }
 }
-NODE* insertBegg(NODE *head,int value){
-    NODE *nptr = (NODE*) malloc(sizeof(NODE));
-    
+// Function to insert a Node at the Beginning of a Linked-List
+NODE *insertBegg(NODE *head, int value)
+{
+    NODE *nptr = (NODE *)malloc(sizeof(NODE));
+
     nptr->data = value;
     nptr->next = head;
     return nptr;
 }
-NODE* insertBTW(NODE *head,int value,int index){
-    NODE *nptr = (NODE*) malloc(sizeof(NODE));
+
+// Function to insert a Node at any given index of a Linked-List
+NODE *insertBTW(NODE *head, int value, int index)
+{
+    NODE *nptr = (NODE *)malloc(sizeof(NODE));
     NODE *temp = head;
     int i = 0;
-    while(i!=index-1){
+    while (i != index - 1)
+    {
         temp = temp->next;
         i++;
     }
@@ -36,54 +45,68 @@ NODE* insertBTW(NODE *head,int value,int index){
     return head;
 }
 
-NODE* insertEnd(NODE *head,int value){
-    NODE *nptr = (NODE*) malloc(sizeof(NODE));
+// Function to insert a Node at the End of a Linked-List
+NODE *insertEnd(NODE *head, int value)
+{
+    NODE *nptr = (NODE *)malloc(sizeof(NODE));
     nptr->data = value;
     NODE *temp = head;
-    while(temp->next != NULL){
-        temp = temp ->next;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
     }
     temp->next = nptr;
     nptr->next = NULL;
     return head;
 }
 
-int main(){
-NODE *head = (NODE*) malloc(sizeof(NODE));
+// Function to insert a Node after a given index of a Linked-List
+NODE *insertAfter(NODE *head, NODE *prevNode, int value)
+{
+    NODE *nptr = (NODE *)malloc(sizeof(NODE));
+    nptr->data = value;
+    nptr->next = prevNode->next;
+    prevNode->next = nptr;
 
-NODE *first = (NODE*) malloc(sizeof(NODE));
+    return head;
+}
 
-NODE *second = (NODE*) malloc(sizeof(NODE));
+int main()
+{
+    // Dynamic memory allocation for all required pointers
+    NODE *head = (NODE *)malloc(sizeof(NODE));
+    NODE *first = (NODE *)malloc(sizeof(NODE));
+    NODE *second = (NODE *)malloc(sizeof(NODE));
+    NODE *third = (NODE *)malloc(sizeof(NODE));
+    NODE *nptr = (NODE *)malloc(sizeof(NODE));
 
-NODE *third = (NODE*) malloc(sizeof(NODE));
+    // Value and next pointer assignment for all nodes
+    head->data = 50;
+    head->next = first;
 
-NODE *nptr = (NODE*) malloc(sizeof(NODE));
+    first->data = 60;
+    first->next = second;
 
+    second->data = 70;
+    second->next = third;
 
+    third->data = 80;
+    third->next = NULL;
 
-head->data = 50;
-head->next = first;
+    // Calling Traversal Function for printing all values in the Linked-List
+    LLTraversal(head);
+    printf("\n");
+    // head = insertBegg(head,40);
+    // printf("Inserted at  First NODE \n");
 
-first->data = 60;
-first->next = second;
+    // head = insertBTW(head,40,2);
+    // printf("Inserted a NODE at INDEX of choice \n");
 
-second->data = 70;
-second->next = third;
+    head = insertAfter(head, second, 69);
+    printf("Inserted a NODE after the given NODE \n");
 
-third->data = 80;
-third->next = NULL;
-
-LLTraversal(head);
-printf("\n");
-// head = insertBegg(head,40);
-// printf("Inserted at  First NODE \n");
-
-// head = insertBTW(head,40,2);
-// printf("Inserted a NODE at INDEX of choice \n");
-head = insertEnd(head,69);
-printf("Inserted a NODE at the END of the LL \n");
-
-LLTraversal(head);
-return 0;
-
+    /*Calling Traversal function again to show the
+    Linked-list with the updated values*/
+    LLTraversal(head);
+    return 0;
 }
