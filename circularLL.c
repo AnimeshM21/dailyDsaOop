53cf2db42138c97e30596ce5fc7ef9a5bb03a91f
AnimeshM21 <animesh.mishra2023@gmail.com>
2024-10-06 03:48:36 +0530

    Circular Linked list

--- circularLL.c
+++ circularLL.c
@@ -1,44 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct
+typedef struct NODE
 {
     int data;
     struct NODE *next;
 } NODE;
 
-void LLTraversal(NODE *ptr,NODE *head)
+void LLTraversal(NODE *head)
 {
-    ptr =  head;
-    while (ptr->next !=head )
+    struct NODE *ptr = head;
+
+    do
     {
         printf("Elements: %d \n", ptr->data);
         ptr = ptr->next;
-    }
-    printf("Elements: %d \n", ptr->data);
+    } while (ptr != head);
 }
 
+struct NODE *insertAtHead(NODE *head, int item)
+{
+    struct NODE *ptr = (struct NODE *)malloc(sizeof(struct NODE));
+    ptr->data = item;
+
+    struct NODE *p = head->next;
+    while (p->next != head)
+    {
+        p = p->next;
+    }
+    p->next = ptr;
+    ptr->next = head;
+    head = ptr;
+    return head;
+}
 int main()
 {
-
     NODE *head = (NODE *)malloc(sizeof(NODE));
+
     NODE *first = (NODE *)malloc(sizeof(NODE));
+
     NODE *second = (NODE *)malloc(sizeof(NODE));
+
     NODE *third = (NODE *)malloc(sizeof(NODE));
 
-    head->data = 10;
+    head->data = 50;
     head->next = first;
 
-    first->data = 20;
+    first->data = 60;
     first->next = second;
 
-    second->data = 30;
+    second->data = 70;
     second->next = third;
 
-    third->data = 40;
+    third->data = 80;
     third->next = head;
+    printf("Circular Linked list before Insertion: \n");
+    LLTraversal(head);
+    printf("Circular Linked list after Insertion: \n");
+    head = insertAtHead(head, 40);
+    LLTraversal(head);
 
-    printf("The Elements of the linked list are as follows:");
-    LLTraversal(head,head);
     return 0;
 }
