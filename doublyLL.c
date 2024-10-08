c504781db724363b6ebf191f35d000b2e1731f2e
AnimeshM21 <animesh.mishra2023@gmail.com>
2024-10-09 03:34:19 +0530

    Doubly Linked List

--- doublyLL.c
+++ doublyLL.c
@@ -7,3 +7,50 @@ typedef struct {
     struct NODE* next;
 }NODE;
 
+void LLTraversalNXT(NODE *head)
+{
+    NODE *ptr = head;
+    while (ptr != NULL)
+    {
+        printf("Element: %d \n", ptr->data);
+        ptr = ptr->next;
+    }
+}
+
+void LLTraversalPRV(NODE *last)
+{
+    NODE *ptr = last;
+    while (ptr != NULL)
+    {
+        printf("Element: %d \n", ptr->data);
+        ptr = ptr->prev;
+    }
+}
+
+int main(){
+NODE* head = (NODE*) malloc(sizeof(NODE));
+NODE* n2 = (NODE*) malloc(sizeof(NODE));
+NODE* n3 = (NODE*) malloc(sizeof(NODE));
+NODE* last = (NODE*) malloc(sizeof(NODE));
+
+head->data = 10;
+head->prev = NULL;
+head->next = n2;
+
+n2->data = 20;
+n2->prev = head;
+n2->next = n3;
+
+n3->data = 30;
+n3->prev = n2;
+n3->next = last;
+
+last->data = 40;
+last->prev = n3;
+last->next = NULL;
+
+printf("Forward Traversing: \n");
+LLTraversalNXT(head);
+printf("Reverse Traversing: \n");
+LLTraversalPRV(last);
+}
