fe719fdcbe211597f07e563444ecd494c9c1f06c
AnimeshM21 <animesh.mishra2023@gmail.com>
2024-10-12 03:04:50 +0530

    Folder updates

--- ReadMeDSA.md
+++
@@ -1,3 +0,0 @@
-<h1> My Daily DSA and OOP practice code Repository</h1>
-
-<p> Just a casual repo to update and record all my college DSA codes and LAB codes with all the practice and revision programs.GGs</p>
---
+++ dsaTrees/linkedBT.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <malloc.h>
+
+typedef struct
+{
+    int data;
+    struct NODE *left;
+    struct NODE *right;
+} NODE;
+
+struct NODE *createNode(int data)
+{
+    struct NODE *newNode;
+    newNode = (struct NODE *)malloc(sizeof(struct NODE));
+    newNode->data = 1;
+    newNode->left = NULL;
+    newNode->right = NULL;
+
+    return newNode;
+}
+
+int main()
+{
+    struct NODE* root = createNode(1);
+    struct NODE* lchild = createNode(2);
+    struct NODE* rchild = createNode(4);
+
+    root->left = lchild;
+    root->right = rchild;
+}
