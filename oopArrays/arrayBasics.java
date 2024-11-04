0b37e717a1aecec85ea47343e0f82263031906c4
AnimeshM21 <animesh.mishra2023@gmail.com>
2024-11-04 15:12:53 +0530

    Arrays in Java'

---
+++ oopArrays/arrayBasics.java
@@ -0,0 +1,25 @@
+public class arrayBasics {
+    public static void main(String args[]) {
+        // Declaration and memory allocation of an array
+        // int[] marks = new int[10];
+        // marks[0] = 10;
+        // marks[1] = 20;
+        // marks[2] = 30;
+        // marks[3] = 40;
+        // marks[4] = 50;
+
+        // Declaration of an array and next line is memory allocation
+        // int[] marks;
+        // new int[10];
+        // marks[0] = 10;
+        // marks[1] = 20;
+        // marks[2] = 30;
+        // marks[3] = 40;
+        // marks[4] = 50;
+
+        // Declaration, memory allocation and initialization of an array
+        int[] marks = { 1, 2, 3, 4, 5 };
+
+        System.out.println(marks[0]);
+    }
+}
