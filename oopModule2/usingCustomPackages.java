d59a7d40f144c3f00476f41e135863773c7be586
AnimeshM21 <animesh.mishra2023@gmail.com>
2024-11-08 04:43:58 +0530

    Module 2 complete

---
+++ oopModule2/usingCustomPackages.java
@@ -0,0 +1,9 @@
+import testpackage.mypackage;
+
+public class usingCustomPackages {
+    public static void main(String args[]){
+        System.out.println("Hi I am using the packages");
+        mypackage mpack = new mypackage();
+        
+    }
+}
