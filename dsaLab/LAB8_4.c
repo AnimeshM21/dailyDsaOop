34f1621696c3d2c8e48600a9bcbd8ccc24a7d496
AnimeshM21 <animesh.mishra2023@gmail.com>
2024-11-13 01:54:40 +0530

    LAB-8.4

--- dsaLab/LAB8_4.c
+++ dsaLab/LAB8_4.c
@@ -4,28 +4,32 @@
 // dequeue() : Remove an item from front of queue.
 // empty() : Checks if a queue is empty or not.
 
-void reverseQueue(Queue* q) {
-if (empty(q)) {
-return;
+// We will have to use a recursive function because we cannot use a doubled ended queue here
+void reverseQueue(Queue *q)
+{
+    if (empty(q))
+    {
+        return;
+    }
+    // Dequeue the front element
+    int x = dequeue(q);
+    // Recursively reverse the remaining queue
+    reverseQueue(q);
+    // Enqueue the dequeued element at the rear
+    enqueue(q, x);
 }
-// Dequeue the front element
-int x = dequeue(q);
-// Recursively reverse the remaining queue
-reverseQueue(q);
-// Enqueue the dequeued element at the rear
-enqueue(q, x);
-}
-int main() {
-Queue q;
-initQueue(&q);
-// Enqueue some elements in the queue
-enqueue(&q, 1);
-enqueue(&q, 2);
-enqueue(&q, 3);
-enqueue(&q, 4);
-enqueue(&q, 5);
-printf("Original queue: \n");
-displayQueue(&q);
+int main()
+{
+    Queue q;
+    initQueue(&q);
+    // Enqueue some elements in the queue
+    enqueue(&q, 1);
+    enqueue(&q, 2);
+    enqueue(&q, 3);
+    enqueue(&q, 4);
+    enqueue(&q, 5);
+    printf("Original queue: \n");
+    displayQueue(&q);
 }
 reverseQueue(&q);
 printf("\nReversed queue: \n");
